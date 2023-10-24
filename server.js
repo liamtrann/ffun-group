dcdd74bb81caccfd19dde17bf72a5a995cc1e173
liamtrann <108684071+liamtrann@users.noreply.github.com>
2023-10-24 18:38:19 -0400

    deploy

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ db.sequelize.sync();
 
 require("./server/routes/car.route")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "client/build/index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
