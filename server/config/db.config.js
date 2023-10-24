d56b9d53c95dcac12f5a55de631481f963e46407
liamtrann <108684071+liamtrann@users.noreply.github.com>
2023-10-24 15:58:35 -0400

    change a live host

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "postgres",
-  PASSWORD: "123",
-  DB: "cardb",
+  PASSWORD: "qUrOVmcBYzMkEUbHVCcH0O8iNSwQFHgB",
+  DB: "	bjtjfrtq",
   dialect: "postgres",
   pool: {
     max: 5,
--- server/models/index.js
+++ server/models/index.js
@@ -1,17 +1,24 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: false,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle,
+//   },
+// });
+
+const url =
+  "postgres://bjtjfrtq:qUrOVmcBYzMkEUbHVCcH0O8iNSwQFHgB@suleiman.db.elephantsql.com/bjtjfrtq";
+
+const sequelize = new Sequelize(process.env.URL_DATABASE || url, {
+  dialect: "postgres",
 });
 
 const db = {};
